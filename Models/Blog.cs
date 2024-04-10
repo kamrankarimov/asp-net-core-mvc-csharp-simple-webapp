f6658f541fd5d6c3796cad4a489a3039f41b2847
kamrankarimov <kamran.vecsiz@gmail.com>
2024-04-10 21:27:05 +0400

    home blog section view component and fake data, model add

---
+++ Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleWebApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ ViewComponents/HomeBlogViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApp.Models;
+using System.Collections.Generic;
+
+namespace SimpleWebApp.ViewComponents
+{
+    public class HomeBlogViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //Demo Data Blog 
+            List<Blog> blog = new List<Blog> {
+                new Blog { 
+                    Id = 1, 
+                    Title = "Test Blog 1", 
+                    Text = "Donec id elit non mi porta gravida at eget metus. Donec sed odio dui.",
+                    Controller = "blog",
+                    Action = "ReadMore",
+                    Category = "article"
+                },
+
+                new Blog {
+                    Id = 2,
+                    Title = "Test Blog 2",
+                    Text = "Donec id elit non mi porta gravida at eget metus. Donec sed odio dui.",
+                    Controller = "blog",
+                    Action = "ReadMore",
+                    Category = "article"
+                },
+
+                new Blog {
+                    Id = 3,
+                    Title = "Test Blog 3",
+                    Text = "Donec id elit non mi porta gravida at eget metus. Donec sed odio dui.",
+                    Controller = "blog",
+                    Action = "ReadMore",
+                    Category = "article"
+                }
+            };
+
+            return View(blog);
+        }
+    }
+}
